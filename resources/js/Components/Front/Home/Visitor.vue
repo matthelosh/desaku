d17cfa551c2b0aa3940d22e5eb9711ce1b7f39e9
matthelosh <matthelosh@gmail.com>
2024-07-25 11:07:40 +0700

    vsiitor component

--- resources/js/Components/Front/Home/Visitor.vue
+++ resources/js/Components/Front/Home/Visitor.vue
@@ -14,6 +14,6 @@ const page = usePage()
             <p>Hari ini: {{ page.props.visitor.today }}</p>
             <p>Semua: {{ page.props.visitor.total }}</p>
             <p>Online: {{ page.props.visitor.online }}</p>
-            </div>
+        </div>
     </div>
 </template>
